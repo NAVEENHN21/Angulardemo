f26a1220b79f9dba615a8a3c19f9a114039a21a1
Naveen H N <you@example.com>
2023-04-28 20:00:41 +0530

    updates

---
+++ QuizApp/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionComponent } from './question/question.component';
+
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'question', component: QuestionComponent },
+
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- QuizApp/src/app/app.component.spec.ts
+++ QuizApp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'online-test-app'`, () => {
+  it(`should have as title 'QuizApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('online-test-app');
+    expect(app.title).toEqual('QuizApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('online-test-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('QuizApp app is running!');
   });
 });
